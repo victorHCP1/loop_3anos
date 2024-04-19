8e91f5dd6752e2762e6a0458ed68264dc11f366a
Victor Hugo Corniani Pereira <125035046+victorHCP1@users.noreply.github.com>
2024-04-19 08:52:07 -0300

    corinthias

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function verificarTime() {
     var respostaTime = prompt("Qual os melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "corinthias") {
+      alert("Isso mesmo! O corinthias é o melhor time!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
